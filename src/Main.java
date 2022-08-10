3de301688a95f09f9d0593f990e048f0a5c6a1d7
AlekseiAnikeev <info.anikeev@gmail.com>
2022-08-11 02:51:01 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Aleksey Anikeev aka AgentChe
+ * Date of creation: 11.08.2022
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,5 @@
+package PACKAGE_NAME;/**
+*@author Aleksey Anikeev aka AgentChe
+*Date of creation: 11.08.2022
+*/public class Person {
+}
