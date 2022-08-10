3a23833759fd880a03ebb34796c1ac9a741bc01f
AlekseiAnikeev <info.anikeev@gmail.com>
2022-08-11 02:52:19 +0300

    add alpha version 0.01a

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.util.*;
+
 /**
  * @author Aleksey Anikeev aka AgentChe
  * Date of creation: 11.08.2022
  */
 public class Main {
     public static void main(String[] args) {
+        Queue<Person> attraction = (Queue<Person>) generateClients();
+        while (!attraction.isEmpty()) {
+            if (attraction.peek().getTicket() > 1) {
+                attraction.peek().deleteTicket();
+                attraction.offer(attraction.poll());
+            } else {
+                attraction.peek().deleteTicket();
+                attraction.poll();
+            }
+        }
+    }
 
+    public static List<Person> generateClients() {
+        List<Person> clients = new LinkedList<>();
+        clients.add(new Person("Иван", "Кудрявцев", new Random().nextInt(5) + 1));
+        clients.add(new Person("Семен", "Никифоров", new Random().nextInt(5) + 1));
+        clients.add(new Person("Николя", "Семенов", new Random().nextInt(5) + 1));
+        clients.add(new Person("Светлана", "Аникеева", new Random().nextInt(5) + 1));
+        clients.add(new Person("Павлунтий", "Василенко", new Random().nextInt(5) + 1));
+        return clients;
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,32 @@
-package PACKAGE_NAME;/**
-*@author Aleksey Anikeev aka AgentChe
-*Date of creation: 11.08.2022
-*/public class Person {
+/**
+ * @author Aleksey Anikeev aka AgentChe
+ * Date of creation: 11.08.2022
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int ticket;
+
+    public Person(String firstName, String lastName, int ticket) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ticket = ticket;
+    }
+
+    public int getTicket() {
+        return ticket;
+    }
+
+    public void deleteTicket() {
+        System.out.println(firstName + " " + lastName + " прокатился на аттракционе, осталось билетов: " + ticket);
+        ticket--;
+    }
+
+    @Override
+    public String toString() {
+        return "Клиент " +
+                firstName +
+                " " + lastName +
+                ", количество билетов: " + ticket;
+    }
 }
